e315f94125afa1d51c2378ba999ec9e9ddd65b9c
sigristarisa <arisasigrist.ch@gmail.com>
2024-10-28 11:59:49 +0100

    created the box array function

--- game.js
+++ game.js
@@ -1,34 +1,55 @@
-// create items that has colors and names (random)
-// create baskets that has names
-// if the color and the name matches in the basket, add points to the counter
-
 let colors = ["#FF0000", "#0000FF", "#00FF00"];
 let names = ["red", "blue", "green"];
-let y = 0;
-let wordArray = [];
+let wordObj = {};
+let boxArray = [];
 
-const createWordObject = () => {
-  const obj = {};
+const createWordObj = () => {
   const name = names[Math.floor(Math.random() * names.length)];
   const color = colors[Math.floor(Math.random() * names.length)];
-  wordArray.push({
-    ...obj,
+  wordObj = {
+    ...wordObj,
     name,
     color,
-    x: Math.floor(Math.random() * window.innerWidth),
-  });
-  console.log("object", wordArray);
+  };
+};
+
+const createBoxArray = () => {
+  const margin = 100;
+  let boxWidth = (windowWidth - margin * 2) / colors.length;
+  let boxX = margin;
+  const boxY = margin * 2;
+  for (let i = 0; i <= colors.length - 1; i++) {
+    const boxObj = {};
+    const color = colors[i];
+    boxArray.push({ ...boxObj, color, boxX, boxY });
+    boxX += boxWidth;
+  }
 };
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  createWordObject();
+  createWordObj();
+  createBoxArray();
+  x = windowWidth / 2;
+  y = windowHeight / 2;
+  textAlign(CENTER, CENTER);
+  console.log("boxArray", boxArray);
 }
 
 function draw() {
   background(0);
   fill(255);
-  fill(wordArray[0].color);
-  text(wordArray[0].name, wordArray[0].x, y);
-  y += 1;
+  fill(wordObj.color);
+  textSize(36);
+  text(wordObj.name, x, y);
+
+  for (let i = 0; i <= boxArray.length - 1; i++) {
+    fill(boxArray[i].color);
+    rect(boxArray[i].boxX, boxArray[i].boxY, 100, 100);
+  }
+}
+
+function mouseDragged() {
+  x = mouseX;
+  y = mouseY;
 }
