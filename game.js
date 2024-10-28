3022b08a995f1b5c920d5ed16f279323b7f07531
sigristarisa <arisasigrist.ch@gmail.com>
2024-10-28 14:38:44 +0100

    created the logic of the game

--- game.js
+++ game.js
@@ -1,11 +1,18 @@
-let colors = ["#FF0000", "#0000FF", "#00FF00"];
-let names = ["red", "blue", "green"];
+let options = [
+  { color: "#FF0000", name: "red" },
+  { color: "#0000FF", name: "blue" },
+  { color: "#00FF00", name: "green" },
+  { color: "#FFA500", name: "orange" },
+  { color: "#FFFF00", name: "yellow" },
+  { color: "#A020F0", name: "purple" },
+];
 let wordObj = {};
 let boxArray = [];
+let points = 0;
 
 const createWordObj = () => {
-  const name = names[Math.floor(Math.random() * names.length)];
-  const color = colors[Math.floor(Math.random() * names.length)];
+  const name = options[Math.floor(Math.random() * options.length)].name;
+  const color = options[Math.floor(Math.random() * options.length)].color;
   wordObj = {
     ...wordObj,
     name,
@@ -15,15 +22,25 @@ const createWordObj = () => {
 
 const createBoxArray = () => {
   const margin = 100;
-  let boxWidth = (windowWidth - margin * 2) / colors.length;
+  let boxWidth = (windowWidth - margin * 2) / options.length;
   let boxX = margin;
-  const boxY = margin * 2;
-  for (let i = 0; i <= colors.length - 1; i++) {
+  const boxY = 200;
+  for (let i = 0; i <= options.length - 1; i++) {
     const boxObj = {};
-    const color = colors[i];
-    boxArray.push({ ...boxObj, color, boxX, boxY });
+    const color = options[i].color;
+    const name = options[i].name;
+    boxArray.push({ ...boxObj, color, name, boxX, boxY });
     boxX += boxWidth;
   }
+};
+
+const rerenderWord = () => {
+  createWordObj();
+  fill(wordObj.color);
+  textSize(36);
+  x = windowWidth / 2;
+  y = windowHeight / 2;
+  text(wordObj.name, x, y);
 };
 
 function setup() {
@@ -33,23 +50,34 @@ function setup() {
   x = windowWidth / 2;
   y = windowHeight / 2;
   textAlign(CENTER, CENTER);
-  console.log("boxArray", boxArray);
 }
 
 function draw() {
   background(0);
   fill(255);
-  fill(wordObj.color);
-  textSize(36);
-  text(wordObj.name, x, y);
-
   for (let i = 0; i <= boxArray.length - 1; i++) {
     fill(boxArray[i].color);
     rect(boxArray[i].boxX, boxArray[i].boxY, 100, 100);
   }
+  fill(wordObj.color);
+  textSize(36);
+  text(wordObj.name, x, y);
+  fill("white");
+  textSize(20);
+  text("Point: " + points, 100, 20);
 }
 
 function mouseDragged() {
   x = mouseX;
   y = mouseY;
+}
+
+function mouseReleased() {
+  console.log("released!!");
+  const droppedBox = boxArray.find(
+    (box) =>
+      box.boxX <= x && box.boxX + (windowWidth - 100 * 2) / options.length >= x
+  );
+  if (droppedBox.name === wordObj.name) points += 1;
+  rerenderWord();
 }
